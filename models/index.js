6f639041d6cd92b1caae5d0b097c26557fd6f2af
Shadow <desclos.julien@gmail.com>
2022-01-17 20:59:42 +0100

    connection a postgres

--- models/index.js
+++ models/index.js
@@ -7,10 +7,14 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
+let option = { 
+  dialectOptions: {
+    ssl: true
+  }
+};
 let sequelize;
 if(env == 'production'){
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, option);
 }else {
   if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
