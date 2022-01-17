83d4fb3b707421a2f5fd67bf2d743cd66934df68
Shadow <desclos.julien@gmail.com>
2022-01-17 21:21:53 +0100

    connection a postgres

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use('/api/', userRoute);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
+console.log(PORT);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
