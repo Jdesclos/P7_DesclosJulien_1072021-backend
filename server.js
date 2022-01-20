b7c3102c991c15f9878b390d680f728616789043
Shadow <desclos.julien@gmail.com>
2022-01-20 10:31:07 +0100

    cors updated

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 const cors=require("cors");
 const corsOptions ={
-   origin:process.env.ORIGIN, 
+   origin:"*", 
    credentials:true,            //access-control-allow-credentials:true
    optionSuccessStatus:200,
 }
