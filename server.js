f9e2008eb0d2f12d6cd72933ae7638127458c643
Shadow <desclos.julien@gmail.com>
2022-01-21 09:53:53 +0100

    add static configuration

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 const cors=require("cors");
 const corsOptions ={
-   origin:"https://groupomaniafrontend.herokuapp.com/", 
+   origin:process.env.ORIGIN, 
    credentials:true,            //access-control-allow-credentials:true
    optionSuccessStatus:200,
 }
